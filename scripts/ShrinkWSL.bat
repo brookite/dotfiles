9958b11e49b1be00000b7f4cd766642b60463efa
Dmitry Shashkov (Brookit) <brookit18@gmail.com>
2026-03-09 20:56:16 +0300

    Update WSL shrinking

--- scripts/ShrinkWSL.bat
+++ scripts/ShrinkWSL.bat
@@ -1,7 +1,29 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set FILES[0]="C:\Users\brookit\AppData\Local\Packages\TheDebianProject.DebianGNULinux_76v4gfsz19hv4\LocalState\ext4.vhdx"
+set FILES[1]="C:\Users\brookit\AppData\Local\Docker\wsl\main\ext4.vhdx"
+set FILES[2]="C:\Users\brookit\AppData\Local\Docker\wsl\disk\docker_data.vhdx"
+set COUNT=3
+
+echo Shutting down WSL...
 wsl --shutdown
-diskpart
-select vdisk file="C:\Users\brookit\AppData\Local\Packages\TheDebianProject.DebianGNULinux_76v4gfsz19hv4\LocalState\ext4.vhdx"
-attach vdisk readonly
-compact vdisk
-detach vdisk
-exit
+
+for /L %%i in (0,1,%COUNT%-1) do (
+    set "FILE=!FILES[%%i]!"
+    echo Processing: !FILE!
+
+    set "TMP_SCRIPT=%TEMP%\diskpart_%%i.txt"
+    (
+        echo select vdisk file="!FILE!"
+        echo attach vdisk readonly
+        echo compact vdisk
+        echo detach vdisk
+        echo exit
+    ) > "!TMP_SCRIPT!"
+
+    diskpart /s "!TMP_SCRIPT!"
+    del "!TMP_SCRIPT!"
+)
+
+echo Done.
--- scripts/WindowsOptimize.bat
+++ scripts/WindowsOptimize.bat
@@ -6,6 +6,4 @@ Dism.exe /Online /Cleanup-Image /StartComponentCleanup /ResetBase
 
 Dism.exe /Online /Cleanup-Image /AnalyzeComponentStore
 
-powershell -c "Optimize-VHD -Path 'C:\Users\brookit\AppData\Local\Packages\TheDebianProject.DebianGNULinux_76v4gfsz19hv4\LocalState\ext4.vhdx' -Mode Full"
-
 pause
